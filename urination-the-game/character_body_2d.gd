18006f65b54be972690d4da60aea0163ff452807
Felippe <felippetm21@gmail.com>
2025-10-03 17:05:02 -0400

    map + joueur + animation

---
+++ urination-the-game/character_body_2d.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+var speed = 400.0;
+ 
+#func _process(delta: float) -> void:
+ 
+ 
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+ 
+const SPEED = 300.0
+ 
+var directionName = "bas";
+ 
+func _physics_process(_delta: float) -> void:
+ 
+	var direction = Vector2(
+		Input.get_axis("gauche", "droite"),
+		Input.get_axis("haut", "bas")
+	)
+ 
+	if direction != Vector2.ZERO:
+		velocity = direction.normalized() * SPEED
+	else:
+		velocity = Vector2.ZERO
+ 
+	if direction != Vector2.ZERO:
+		if abs(direction.x) > abs(direction.y):
+			if direction.x > 0:
+				directionName = "droite";
+			else:
+				directionName = "gauche";
+		else:
+			if direction.y < 0:
+				directionName = "haut";
+			else:
+				directionName = "bas";
+	if direction == Vector2.ZERO:
+		if directionName == "haut":
+			animation.play("animation_idle");
+	else:
+		animation.play("animation_" + directionName);
+ 
+	move_and_slide()
